21bf83043b85197cdc299a11fa9ccc1ea6a26e90
ELLIE SMITH <ES296746@FALMOUTH.AC.UK>
2025-06-18 15:35:59 +0100

    changed speed stages to 4 instead of 5 and added colour to indicator

--- Assets/Scripts/AmmoSpeedScript/PlayerSpeedManager.cs
+++ Assets/Scripts/AmmoSpeedScript/PlayerSpeedManager.cs
@@ -30,40 +30,37 @@ public class PlayerSpeedManager : MonoBehaviour
         gunScript = GameObject.FindWithTag("Weapon").GetComponent<GunScript>();
         float currentAmmo = gunScript.bulletsInTheGun;
 
-        if (currentAmmo > 64)
-        {
-            currentSpeedBoost = 5;
-            pms.accelerationSpeed = 4000000;
-            //pms.maxSpeed = 5000;
-            speedBoostIndicator.text = "Speed Boost: " + currentSpeedBoost.ToString() + "x";
-        }
-        else if (currentAmmo > 48 && currentAmmo < 64)
+        if (currentAmmo > 70)
         {
             currentSpeedBoost = 4;
-            pms.accelerationSpeed = 3500000;
+            pms.accelerationSpeed = 4000000;
             //pms.maxSpeed = 4000;
             speedBoostIndicator.text = "Speed Boost: " + currentSpeedBoost.ToString() + "x";
+            speedBoostIndicator.color = Color.green;
         }
-        else if (currentAmmo > 32 && currentAmmo < 48)
+        else if (currentAmmo > 60 && currentAmmo < 70)
         {
             currentSpeedBoost = 3;
             pms.accelerationSpeed = 3000000;
             //pms.maxSpeed = 3000;
             speedBoostIndicator.text = "Speed Boost: " + currentSpeedBoost.ToString() + "x";
+            speedBoostIndicator.color = Color.green;
         }
-        else if (currentAmmo > 16 && currentAmmo < 32)
+        else if (currentAmmo > 30 && currentAmmo < 60)
         {
             currentSpeedBoost = 2;
             pms.accelerationSpeed = 1000000;
             //pms.maxSpeed = 2000;
             speedBoostIndicator.text = "Speed Boost: " + currentSpeedBoost.ToString() + "x";
+            speedBoostIndicator.color = Color.yellow;
         }
-        else if (currentAmmo < 16)
+        else if (currentAmmo < 30)
         {
             currentSpeedBoost = 1;
             pms.accelerationSpeed = 500000;
             //pms.maxSpeed = 1000;
             speedBoostIndicator.text = "Speed Boost: " + currentSpeedBoost.ToString() + "x";
+            speedBoostIndicator.color = Color.red;
         }
     }
 }
